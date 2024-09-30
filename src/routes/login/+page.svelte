c1843ca69a1103a7c83dda744e8a9f00c7b1a567
Toby Hobson <toby.hobson@gmail.com>
2024-09-30 15:12:03 +0100

    Tutorial part 3

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,14 +1,17 @@
 <!-- src/routes/login/+page.svelte -->
 <script lang="ts">
-  import { enhance } from '$app/forms'
+  import { applyAction, enhance } from '$app/forms'
   import { Passlock, PasslockError } from '@passlock/sveltekit'
   import type { SubmitFunction } from './$types'
+  import Spinner from '$lib/Spinner.svelte'
+  import { onMount } from 'svelte'
 
   import { 
     PUBLIC_PASSLOCK_TENANCY_ID, 
     PUBLIC_PASSLOCK_CLIENT_ID,
     PUBLIC_PASSLOCK_ENDPOINT
   } from '$env/static/public'
+    import { goto } from '$app/navigation';
 
   const passlock = new Passlock({ 
     tenancyId: PUBLIC_PASSLOCK_TENANCY_ID, 
@@ -16,21 +19,45 @@
     endpoint: PUBLIC_PASSLOCK_ENDPOINT
   })
 
+  let requestPending = false
+
   const onSubmit: SubmitFunction = async ({ cancel, formData }) => {
+    // show the spinner when the request is made
+    requestPending = true
     const email = formData.get('email') as string
 
     const user = await passlock.authenticatePasskey({ email })
 
     if (!PasslockError.isError(user)) {
+      // if we can't login using a passkey, abort
+      requestPending = false      
       formData.set('token', user.token)
     } else {
       cancel() // prevent form submission
       alert(user.message)
     }
+
+    return async ({ result }) => {
+      // form action completed so hide the spinner
+      requestPending = false
+      if (result.type === 'redirect') {
+        goto(result.location)
+      } else {
+        await applyAction(result)
+      }
+    }    
   }
+
+  onMount(async () => {
+    await passlock.preConnect()
+  })
 </script>
 
 <form method="post" use:enhance={onSubmit}>
   Email: <input type="text" name="email" /> <br />
   <button type="submit">Login</button>
+
+  {#if requestPending}
+    <Spinner />
+  {/if}
 </form>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,46 +1,68 @@
-<!-- src/routes/register/+page.svelte -->
 <script lang="ts">
-  import { enhance } from '$app/forms'
-  import type { SubmitFunction } from './$types'
+  import { applyAction, enhance } from '$app/forms'
   import { Passlock, PasslockError } from '@passlock/sveltekit'
+  import type { SubmitFunction } from './$types'
+  import { goto } from '$app/navigation'
+  import Spinner from '$lib/Spinner.svelte'
+  import { onMount } from 'svelte'
 
   import { 
     PUBLIC_PASSLOCK_TENANCY_ID, 
-    PUBLIC_PASSLOCK_CLIENT_ID,
-    PUBLIC_PASSLOCK_ENDPOINT
+    PUBLIC_PASSLOCK_CLIENT_ID 
   } from '$env/static/public'
 
   const passlock = new Passlock({ 
     tenancyId: PUBLIC_PASSLOCK_TENANCY_ID, 
-    clientId: PUBLIC_PASSLOCK_CLIENT_ID,
-    endpoint: PUBLIC_PASSLOCK_ENDPOINT
+    clientId: PUBLIC_PASSLOCK_CLIENT_ID, 
+    endpoint: 'https://okbq1o3xde.execute-api.eu-west-2.amazonaws.com' 
   })
 
-  // during form submission, ask Passlock to register a
-  // passkey on the user's device. This will return a 
-  // secure token, representing the newly created passkey
-  const registerPasskey: SubmitFunction = async ({ cancel, formData }) => {
+  let requestPending = false
+
+  const onSubmit: SubmitFunction = async ({ formData, cancel }) => {
+    // show the spinner when the request is made
+    requestPending = true
     const email = formData.get('email') as string
     const givenName = formData.get('givenName') as string
     const familyName = formData.get('familyName') as string
 
-    const user = await passlock.registerPasskey({ 
-      email, givenName, familyName 
+    const result = await passlock.registerPasskey({ 
+      email, givenName, familyName
     })
-
-    if (!PasslockError.isError(user)) {
-      // attach the token to the request
-      formData.set('token', user.token)
+    
+    if (PasslockError.isError(result)) {
+      // if we can't register a passkey, abort
+      requestPending = false
+      cancel()
+      alert(result.message)
     } else {
-      cancel() // prevent form submission
-      alert(user.message)
+      formData.set('token', result.token)
+    }
+
+    return async ({ result }) => {
+      // form action completed so hide the spinner
+      requestPending = false
+      if (result.type === 'redirect') {
+        goto(result.location)
+      } else {
+        await applyAction(result)
+      }
     }
   }
+
+  onMount(async () => {
+    await passlock.preConnect()
+  })
 </script>
 
-<form method="post" use:enhance={registerPasskey}>
+<form method="post" use:enhance={onSubmit}>
   Email: <input type="text" name="email" /> <br />
   First name: <input type="text" name="givenName" /> <br />
   Last name: <input type="text" name="familyName" /> <br />
+  <input type="hidden" name="token" />
   <button type="submit">Register</button>
+
+  {#if requestPending}
+    <Spinner />
+  {/if}
 </form>
